f058b1e77267b932779505db5b848b3916ffaca8
Lucas E. Dickmann <131892016+ldickmann@users.noreply.github.com>
2025-01-26 00:23:56 -0300

    Atualização vite.vonfig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// filepath: /C:/Users/ldick/repositorios_github/newPortfolio/vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/newPortfolio/",
   plugins: [react()],
-})
+});
