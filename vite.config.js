201da1af444623dcd089e7de2b4acb8c8805f380
Lucas E. Dickmann <131892016+ldickmann@users.noreply.github.com>
2025-01-26 00:37:12 -0300

    Fix deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// filepath: /C:/Users/ldick/repositorios_github/newPortfolio/vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/newPortfolio/",
+  base: '/newPortfolio/',
   plugins: [react()],
 });
