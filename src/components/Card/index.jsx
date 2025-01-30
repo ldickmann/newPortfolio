2753307bff9a2aed846b6e62af133f480372cd3e
Lucas E. Dickmann <131892016+ldickmann@users.noreply.github.com>
2025-01-30 17:10:13 -0300

    Novo componente Carousel e atualização na responsividade de Card componente

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import {
   FaPython,
   FaJs,
@@ -8,7 +9,7 @@ import {
 } from "react-icons/fa";
 import { DiDjango } from "react-icons/di";
 import { SiQuasar } from "react-icons/si";
-import styled from "styled-components";
+import Carousel from "../Carousel";
 
 const CardStyles = styled.div`
   display: flex;
@@ -17,28 +18,57 @@ const CardStyles = styled.div`
   gap: 4rem;
   margin: 0 auto;
   padding: 2rem 0rem 2rem 0rem;
-  background-color: #433d3d;
+  background-color: var(--color-beige);
   border-radius: 10px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.723);
   width: 80%;
 
-  @media (max-width: 768px) {
-    gap: 2rem;
+  @media (max-width: 769px) {
+    display: none;
+  }
+`;
+
+const CarouselWrapper = styled.div`
+  display: none;
+  background-color: var(--color-beige);
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.723);
+  width: 80%;
+  gap: 0rem;
+  margin: 0 auto;
+  padding: 2rem 0rem 2rem 0rem;
+
+  @media (max-width: 769px) {
+    display: block;
   }
 `;
 
 const Card = () => {
   return (
-    <CardStyles>
-      <FaPython size={80} color="white" />
-      <FaJs size={80} color="white" />
-      <FaVuejs size={80} color="white" />
-      <FaReact size={80} color="white" />
-      <DiDjango size={80} color="white" />
-      <SiQuasar size={80} color="white" />
-      <FaSass size={80} color="white" />
-      <FaBootstrap size={80} color="white" />
-    </CardStyles>
+    <>
+      <CardStyles>
+        <FaPython size={80} color="white" />
+        <FaJs size={80} color="white" />
+        <FaVuejs size={80} color="white" />
+        <FaReact size={80} color="white" />
+        <DiDjango size={80} color="white" />
+        <SiQuasar size={80} color="white" />
+        <FaSass size={80} color="white" />
+        <FaBootstrap size={80} color="white" />
+      </CardStyles>
+      <CarouselWrapper>
+        <Carousel>
+          <FaPython size={80} color="white" />
+          <FaJs size={80} color="white" />
+          <FaVuejs size={80} color="white" />
+          <FaReact size={80} color="white" />
+          <DiDjango size={80} color="white" />
+          <SiQuasar size={80} color="white" />
+          <FaSass size={80} color="white" />
+          <FaBootstrap size={80} color="white" />
+        </Carousel>
+      </CarouselWrapper>
+    </>
   );
 };
 
